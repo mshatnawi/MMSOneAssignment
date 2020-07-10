c681b45e5b06a9925182f623272b211c8b0c7007
Mohamad <shatnawi.mohamad@gmail.com>
2020-07-10 13:45:03 +0200

    - minor modifications 2

--- MMSOneAssignmentWebsite/assets/js/main.js
+++ MMSOneAssignmentWebsite/assets/js/main.js
@@ -47,74 +47,81 @@ document.getElementById('btn_drawGrid').addEventListener('click', function (evt)
         //hide the solution holder object (DIV)
         solutionHolder.style.display = "none";
         //clear any created grid (context)
-        context.clearRect(0, 0, initialCanvas.width, initialCanvas.height);
+        initialContext.clearRect(0, 0, initialCanvas.width, initialCanvas.height);
         //remove the on click event listener of the initial canvas
         initialCanvas.removeEventListener('click', onCanvasClick, false);
         //resize the initial canvas to 0.
         initialCanvas.width = 0;
         initialCanvas.height = 0;
+        alert("Grid Size must be between 5 and 1000");
     }
 }, false);
 
 // add event listner to "Solve" button
 document.getElementById('btn_solve').addEventListener('click', function (evt) {
-    //define the html string that will be injected to the solution holder (DIV)
-    var htmlVal = "";
-    //define the size of the result canvases to be the same as the initial canvas
-    var canvasSize = gridSize.value * 10 + 1;
-    //define GameOfLife object
-    var gol = new GameOfLife();
-    //initiate the GOL object -- this is a constructor function
-    gol.Initiate(gridSize.value, genCount.value);
-    //fill the initialgrid (generation 0) with the selected values
-    gol.FillInitialGrid(selectedCells);
-    //solve the game of life
-    gol.Solve();
-    //save the game of life results to a new object
-    var results = gol.GetResultsArray();
-    //loop on each generation based on Generation Count textbox value
-    for (i = 1; i <= genCount.value; i++) {
-        //define the injected html as string
-        htmlVal += "<div class='row'><div class='col-md-12'><hr /></div>"
-        htmlVal += "<div class='col-md-12 text-center'><label>Generation " + i + "</label></div>";
-        htmlVal += "<div class='col-md-12 text-center' style='overflow: auto; max-height:400px'>";
-        //define variable ID of each canvas, which later will be filled with the result grid of each generation based on its ID
-        htmlVal += "<canvas id='cnv_solution" + i + "' width='" + canvasSize + "' height='" + canvasSize + "'></canvas>";
-        htmlVal += "</div></div><br />";
-    }
-    //inject the html into the solution holder (DIV)
-    //loop on each generation (starting from generation 1, where generation 0 is the initial grid)_
-    //based on Generation Count textbox value.It can be done on the previous loop, but separated to be more clear
-    //the generations count (include generation 0) is the same length as the first dimension in the results array
-    solutionHolder.innerHTML = htmlVal;
-    for (i = 1; i <= genCount.value; i++) {
-        //define a temporary canvas which has the id of the generation (similar to the one defined in the previous loop)
-        var canvasTemp = document.getElementById('cnv_solution' + i);
-        var contextTemp = canvasTemp.getContext('2d');
-        //draw a grid inside the temporary canvas object
-        drawGrid(contextTemp, gridSize.value);
-        //loop on the second dimension in results array
-        for (j = 0; j < results[i].length; j++) {
-            //loop on the third dimension in results array
-            for (k = 0; k < results[i][j].length; k++) {
-                //fill the cell if the value of the item in the third dimension is true (alive)
-                if (results[i][j][k]) {
-                    //again, each cell is 10px length and width
-                    var x = 10 * (j + 0.1);
-                    var y = 10 * (k + 0.1);
-                    //the rows (x) in the array are horizontal, and the columns (y) in the array are vertical on the secreen (flipped)
-                    fillSquare(contextTemp, y, x);
-                }
-
-
-            }
+    //check if generations count is not empty
+    if (genCount.value !== "") {
+        //define the html string that will be injected to the solution holder (DIV)
+        var htmlVal = "";
+        //define the size of the result canvases to be the same as the initial canvas
+        var canvasSize = gridSize.value * 10 + 1;
+        //define GameOfLife object
+        var gol = new GameOfLife();
+        //initiate the GOL object -- this is a constructor function
+        gol.Initiate(gridSize.value, genCount.value);
+        //fill the initialgrid (generation 0) with the selected values
+        gol.FillInitialGrid(selectedCells);
+        //solve the game of life
+        gol.Solve();
+        //save the game of life results to a new object
+        var results = gol.GetResultsArray();
+        //loop on each generation based on Generation Count textbox value
+        for (i = 1; i <= genCount.value; i++) {
+            //define the injected html as string
+            htmlVal += "<div class='row'><div class='col-md-12'><hr /></div>"
+            htmlVal += "<div class='col-md-12 text-center'><label>Generation " + i + "</label></div>";
+            htmlVal += "<div class='col-md-12 text-center' style='overflow: auto; max-height:400px'>";
+            //define variable ID of each canvas, which later will be filled with the result grid of each generation based on its ID
+            htmlVal += "<canvas id='cnv_solution" + i + "' width='" + canvasSize + "' height='" + canvasSize + "'></canvas>";
+            htmlVal += "</div></div><br />";
         }
+        //inject the html into the solution holder (DIV)
+        //loop on each generation (starting from generation 1, where generation 0 is the initial grid)_
+        //based on Generation Count textbox value.It can be done on the previous loop, but separated to be more clear
+        //the generations count (include generation 0) is the same length as the first dimension in the results array
+        solutionHolder.innerHTML = htmlVal;
+        for (i = 1; i <= genCount.value; i++) {
+            //define a temporary canvas which has the id of the generation (similar to the one defined in the previous loop)
+            var canvasTemp = document.getElementById('cnv_solution' + i);
+            var contextTemp = canvasTemp.getContext('2d');
+            //draw a grid inside the temporary canvas object
+            drawGrid(contextTemp, gridSize.value);
+            //loop on the second dimension in results array
+            for (j = 0; j < results[i].length; j++) {
+                //loop on the third dimension in results array
+                for (k = 0; k < results[i][j].length; k++) {
+                    //fill the cell if the value of the item in the third dimension is true (alive)
+                    if (results[i][j][k]) {
+                        //again, each cell is 10px length and width
+                        var x = 10 * (j + 0.1);
+                        var y = 10 * (k + 0.1);
+                        //the rows (x) in the array are horizontal, and the columns (y) in the array are vertical on the secreen (flipped)
+                        fillSquare(contextTemp, y, x);
+                    }
 
+
+                }
+            }
+
+        }
+        //show the solution holder object (DIV)
+        solutionHolder.style.display = "";
+        alert("Solved!");
+        window.scroll(0, 450);
     }
-    //show the solution holder object (DIV)
-    solutionHolder.style.display = "";
-    alert("Solved!");
-    window.scroll(0, 450);
+    else {
+        alert("Please specifiy the Generations Count");
+    }
 }, false);
 
 //create onCanvasClick function, to color the selected cell in the initial canvas
